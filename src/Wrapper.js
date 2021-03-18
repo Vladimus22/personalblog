1b12ee4ba67bd1e4684103f5059a88e648861135
Vladimus22 <68963869+Vladimus22@users.noreply.github.com>
2021-03-18 18:53:38 +0300

    alpha version

---
+++ src/Wrapper.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Wrapper.css';
+import avatar_2 from './img/avatar_2.svg';
+
+function Wrapper() {
+    const user = {
+        firstName: 'Vladyslav',
+        secondName: 'Filenko'
+    }
+    return(
+        <div className="wrapper">
+            <div className="wrapper-profile">
+                <img className="avatar" src= {avatar_2} alt ='avatar'></img>
+                <div className="user-name">{user.firstName} {user.secondName}</div>
+                <div className="user-status">七転び八起き</div>
+                <div className="wrapper-buttons">
+                    <button className="user-buttons" id="user-buttons-projects">
+                        <div className="count">0</div>
+                        <div className="count-title">Projects</div>
+                    </button>
+                    <button className="user-buttons" id="user-buttons-articles">
+                        <div className="count">1</div>
+                        <div className="count-title">Articles</div>
+                    </button>
+                    <button className="user-buttons" id="user-buttons-drawing">
+                        <div className="count">0</div>
+                        <div className="count-title">Drawing</div>
+                    </button>
+                </div>
+            </div>
+            <div className="wrapper-articles">
+                <article>
+                    <h1 id="jp">
+                        こんにちは！
+                    </h1>
+                    <h4>18 March, 2020</h4>
+                    <p>どういたしまして。</p>
+                    
+                </article>
+            </div>
+        </div>
+    );
+}
+
+export default Wrapper;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Wrapper from './Wrapper';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode>   
+    <Wrapper />
   </React.StrictMode>,
   document.getElementById('root')
 );
